9044f2c783dc490cf3e0fb720d61d86ae3631ed6
alexsameri <samerialexx@gmail.com>
2023-01-03 08:02:53 -0800

    Task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("first three letters: {}".format(middle_word))
-print("last two letters: {}".format(word_last_2))
-print("middle word: {}".format(middle_word)
+print("First three letters: {}".format(middle_word))
+print("Last two letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word)
