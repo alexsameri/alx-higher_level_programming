88d34dc9980fef3df7e27e273757ce94f2887283
samerialex <samerialexx@gmail.com>
2023-01-23 19:25:01 +0000

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     """function that divides 2 integers and prints the result
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        value of the division,
-        Otherwise - None
     """
     try:
         div = a / b
@@ -14,3 +8,4 @@ def safe_print_division(a, b):
         div = None
     finally:
         print("Inside result: {}".format(div))
+    return (div)
