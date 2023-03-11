161039aa9d3a908f9bd93e3521bf108d0bd59f7c
samerialex <samerialexx@gmail.com>
2023-03-11 21:33:03 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the class Square"""
+class Square:
+    """Defines the Square by its size"""
+    def __init__(self, size):
+        """Initializes the Square ny its size"""
+        self._size = size
