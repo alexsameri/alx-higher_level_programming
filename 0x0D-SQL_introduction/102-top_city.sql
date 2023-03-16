1220b11cc3451b92a87bb83439af27f958f82fb2
alexsameri <samerialexx@gmail.com>
2023-03-16 02:25:08 -0700

    Tasks

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- Displays the top 3 of cities temperatures during July and August
+
+
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
