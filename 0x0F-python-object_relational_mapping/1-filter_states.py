8cad5959f89dbed76c6ee9a923b0a251ae0557e9
alexsameri <samerialexx@gmail.com>
2023-04-15 11:53:51 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        # Get the arguments
-        mysql_user = sys.argv[1]
-        mysql_password = sys.argv[2]
-        db_name = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
 
-        # Connect to the database
-        db = MySQLdb.connect(
-            host='localhost',
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
             port=3306,
-            user=mysql_user,
+            user=mysql_username,
             passwd=mysql_password,
-            db=db_name
+            db=db_name,
+            charset="utf8"
         )
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
 
-        # Get a cursor to execute queries
-        cursor = db.cursor()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+    rows = cur.fetchall()
 
-        # Execute the query
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for row in rows:
+        print(row)
 
-        # Fetch the results
-        results = cursor.fetchall()
-
-        # Print the results
-        for row in results:
-            print(row)
-
-        # Close the database connection
-        db.close()
-
-    else:
-        # Print the usage instructions
-        print("Usage: {} mysql_user mysql_password db_name".format(sys.argv[0]))
+    cur.close()
+    conn.close()
